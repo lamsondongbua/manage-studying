8cd4489fccf4e3f501beb7832ac9b84d1cf76d08
lamsondongbua <dungxoi18072004@gmail.com>
2025-12-22 17:16:40 +0700

    Completed project 2

--- FrontEnd/next.config.mjs
+++ FrontEnd/next.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+  output: "export", // Thêm dòng này
+  typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true },
   reactStrictMode: false,
 };
-
-export default nextConfig
+export default nextConfig;
